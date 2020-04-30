5544561ba0c66b80ef6ddf6dd55b873cf7543879
Chris Grandin <chris.grandin@gmail.com>
2020-04-30 16:14:30 -0700

    Add biasadjust mse running

--- R/run_MSE_biasadj.R
+++ R/run_MSE_biasadj.R
@@ -1,48 +1,30 @@
 library(PacifichakeMSE)
-library(purrr)
 library(here)
-library(r4ss)
-.seed <- 12345
-results_dir <- "bias_adjustment"
-nruns <- 2
-simyears <- 3
+library(purrr)
+library(stringr)
 
-mod <- SS_output(system.file("extdata/SS32018/",
-                             package = "PacifichakeMSE",
-                             mustWork = TRUE),
-                 printstats = FALSE,
-                 verbose = FALSE)
+results_root_dir <- "results"
+results_dir <- "biasadjust"
+fns <- c("MSErun_move_nofishing_nobiasadj",
+         "MSErun_move_nofishing_biasadj",
+         "MSErun_move_nofishing_biasadj_med")
 
-seeds <- floor(runif(n = nruns, min = 1, max = 1e6))
-fns <- c("MSErun_move_nofishing_nobiasadj.rds",
-         "MSErun_move_nofishing_biasadj.rds",
-         "MSErun_move_nofishing_biasadj_med.rds")
-if(!dir.exists(here("results"))){
-  dir.create(here("results"))
-}
-if(!dir.exists(here("results", results_dir))){
-  dir.create(here("results", results_dir))
-}
-
-# Prepare data for operating model
-dfs <- list()
-dfs[[1]] <- load_data_seasons(nseason = 4,
-                              nspace = 2)
-dfs[[2]] <- load_data_seasons(nseason = 4,
-                              nspace = 2,
-                              bfuture = 0.87)
-dfs[[3]] <- load_data_seasons(nseason = 4,
-                              nspace = 2,
-                              bfuture = 0.5)
-
-# Loop MSEs with different errors in future survey and recruitment
-map2(fns, dfs, ~{
-  ls_save <- map(1:nruns, function(run = .x, ...){
-    tmp <- run_multiple_OMs(simyears = simyears,
-                            seed = seeds[run],
-                            df = .y,
-                            Catchin = 0)
-    if(is.list(tmp)) tmp else NA
-  }, ...)
-  saveRDS(ls_save, file = here("results", results_dir, .x))
-})
+run_mses(ss_extdata_dir = "SS32018",
+         nruns = 2,
+         simyears = 3,
+         fns = fns,
+         om_params_seed = 12345,
+         multiple_season_data = list(c(nseason = 4,
+                                       nspace = 2,
+                                       bfuture = 0),
+                                     c(nseason = 4,
+                                       nspace = 2,
+                                       bfuture = 0.87),
+                                     c(nseason = 4,
+                                       nspace = 2,
+                                       bfuture = 0.5)),
+         results_root_dir = results_root_dir,
+         results_dir = results_dir,
+         nseason = 4,
+         nspace = 2,
+         bfuture = 0.5)
