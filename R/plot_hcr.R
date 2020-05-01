421e0bd8dcdc089fbd147145227816daa516245a
Chris Grandin <chris.grandin@gmail.com>
2020-05-01 01:50:55 -0700

    Add violin plot

--- R/plot_hcr.R
+++ R/plot_hcr.R
@@ -11,5 +11,4 @@ ps_hcr <- setup_mse_plot_objects(results_dir = here::here("results/hcr"),
 plot_bars_country(ps_hcr)
 plot_bars_country_season(ps_hcr)
 plot_violins_country(ps_hcr)
-
-
+plot_violins(ps_hcr)
