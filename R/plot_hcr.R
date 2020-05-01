70320443f3ac2fb0ad93068b00889de38f1fc5f3
Chris Grandin <chris.grandin@gmail.com>
2020-05-01 14:47:25 -0700

    Add several examples of how the setup_mse_plot_objects() functin works

--- R/plot_hcr.R
+++ R/plot_hcr.R
@@ -3,12 +3,14 @@ library(reshape2)
 library(ggplot2)
 
 # This will assign plotnames to what they were when set up before running the MSE
+ps_hcr <- setup_mse_plot_objects(results_dir = here::here("results/hcr"))
+
+# This will re-order the scenarios in the plot
 ps_hcr <- setup_mse_plot_objects(results_dir = here::here("results/hcr"),
-                                 plotexp = TRUE)
+                                 porder = c(2, 1, 4, 3))
 
 # This is how you override the original plotnames and use custom ones
 ps_hcr <- setup_mse_plot_objects(results_dir = here::here("results/hcr"),
-                                 plotexp = TRUE,
                                  plotnames = c("A",
                                                "B",
                                                "C",
