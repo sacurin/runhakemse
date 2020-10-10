a58224469917eb24f188f66b05bd03e8068ac639
Chris Grandin <chris.grandin@gmail.com>
2020-10-10 00:04:24 -0700

    Clen up run_om_only script

--- R/run_om_only.R
+++ R/run_om_only.R
@@ -1,18 +1,21 @@
 load_all("../pacifichakemse/")
 
-# Number of years to simulate into the future
-n_sim_yrs <- 4
 
-ss_model_output_dir <- "C:/tmp/PacifichakeMSE/inst/extdata/SS32018"
+#ss_model_output_dir <- "C:/tmp/PacifichakeMSE/inst/extdata/SS32018"
+ss_model_output_dir <- "C:/github/pacific-hake/hake-assessment/models/2020.01.09_DMprior_base"
+
+ss_model_data_csv_dir <- "C:/github/pacific-hake/hake-assessment/data"
 ss_model_raw <- load_ss_model_from_rds(ss_model_output_dir,
+                                       ss_model_data_csv_dir,
                                        load_extra_mcmc = FALSE)
-ss_model <- load_ss_model_data(ss_model_raw,
-                               s_yr = 1966,
-                               m_yr = 2018)
-om <- load_data_om(ss_model,
-                   n_sim_yrs,
-                   catch_props_space_season = list(c(0.001, 0.188, 0.603, 0.208),
-                                                       c(0.000, 0.317, 0.382, 0.302)))
-om <- run_om(om,
-             random_seed = 42,
-             verbose = FALSE)
+ss_model <- load_ss_model_data(ss_model_raw)
+om <- load_data_om(ss_model, n_sim_yrs = 5)
+om_0 <- run_om(om, verbose = FALSE)
+#om$selectivity_change <- 1
+#om_1 <- run_om(om, verbose = FALSE)
+#om$selectivity_change <- 2
+#om_2 <- run_om(om, verbose = FALSE)
+cat(green(symbol$tick),
+    green("Ran OM successfully\n"))
+
+
