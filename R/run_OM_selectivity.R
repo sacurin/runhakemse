69b8c9510033a3d497927932dcba6067e87cdc4f
Chris Grandin <chris.grandin@gmail.com>
2020-10-14 02:25:03 -0700

    Modify and delete files to work with new pacifichakemse

--- R/run_OM_selectivity.R
+++ R/run_OM_selectivity.R
@@ -1,39 +1,51 @@
-### Run MSE_all_OM ##
+load_all("../pacifichakemse/")
 
-### Re-run the operating model for the selectivity scenario
+ss_model_yr <- 2018
+ss_model_output_dir <- file.path(system.file(package = "pacifichakemse", mustWork = TRUE),
+                                 "extdata", paste0("SS3_", ss_model_yr))
+ss_model_data_csv_dir <- "C:/github/pacific-hake/hake-assessment/data"
+ss_model_raw <- load_ss_model_from_rds(ss_model_output_dir,
+                                       ss_model_data_csv_dir,
+                                       load_extra_mcmc = FALSE,
+                                       overwrite_ss_rds = TRUE)
+ss_model <- load_ss_model_data(ss_model_raw)
+om <- load_data_om(ss_model, n_sim_yrs = 5)
 
-library(TMB)
-compile("runHakeassessment.cpp")
-dyn.load(dynlib("runHakeassessment"))
+om$selectivity_change <- 0
+om_0 <- run_om(om, verbose = FALSE)
+stop()
+om$selectivity_change <- 1
+om_1 <- run_om(om, verbose = FALSE)
 
-seedz <- 12345
-set.seed(seedz)
+om$selectivity_change <- 2
+om_2 <- run_om(om, verbose = FALSE)
 
-df <- load_data_seasons(nseason = 4, nspace = 2, bfuture = 0.5, selectivity_change = 2) # Prepare data for operating model
+cat(green(symbol$tick),
+    green("Ran OM successfully\n"))
+stop()
+# Check the three OMs - final year selectivity at age
+offset <- 0 # How many years prior to the final year
+yr_ind <- dim(om_0$f_sel_save)[2] - offset
+yr <- om_0$m_yr - offset
+d <- data.frame(sel = c(om_0$f_sel_save[,yr_ind,1], om_1$f_sel_save[,yr_ind,1], om_2$f_sel_save[,yr_ind,1],
+                        om_0$f_sel_save[,yr_ind,2], om_1$f_sel_save[,yr_ind,2], om_2$f_sel_save[,yr_ind,2]),
+                country = rep(c("CAN", "USA"), each = 3 * om_0$n_age),
+                age = rep(om_0$ages, 6),
+                run = rep(rep(c("Base scenario",
+                                "Low US selectivity",
+                                paste0(yr, " selectivity")),
+                              each = om_0$n_age), 2)) %>%
+  as_tibble()
+d$run <- factor(d$run, levels = c("Base scenario",
+                                  "Low US selectivity",
+                                  paste0(yr, " selectivity")))
 
-time <- 1
-yrinit <- df$nyear
-nruns <- 100
-seeds <- floor(runif(n = nruns, min = 1, max = 1e6))
+p <- ggplot(data = d,
+            aes(x = age, y = sel, color = country)) +
+  theme_classic() +
+  geom_line(size = 1.2) +
+  facet_wrap(~run) +
+  scale_x_continuous(limit = c(0, om_0$age_max_age)) +
+  scale_color_manual(values = c("darkred","blue4"))
 
-simyears <- 30 # Project 25 years into the future (2048 that year)
-year.future <- c(df$years,(df$years[length(df$years)]+1):(df$years[length(df$years)]+simyears))
-N0 <- NA
-sim.data <- run.agebased.true.catch(df)
-simdata0 <- sim.data # The other one is gonna get overwritten.
-
-folders <- 'results/Selectivity/'
-
-dts <- dir(folders)[grep('.Rdata',x = dir(folders))]
-
-
-load(paste(folders,dts[1], sep = ''))
-df.1 <- ls.save
-
-OM.1 <- list()
-
-
-for(i in 1:nruns){
-OM.1[[i]] <- run_multiple_OMs(simyears,seed = seeds[i],df = df,Catchin = df.1[[i]]$Catch )
-
-}
+print(p)
