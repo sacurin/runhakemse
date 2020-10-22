9527acb3b76c5cf24bf455ac030536950973621b
Chris Grandin <chris.grandin@gmail.com>
2020-10-22 04:36:17 -0700

    Modify bias adjust MSE script

--- R/run_MSE_biasadj.R
+++ R/run_MSE_biasadj.R
@@ -1,10 +1,15 @@
-library(PacifichakeMSE)
+#library(pacifichakemse)
+load_all("../pacifichakemse")
 library(here)
-library(purrr)
-library(stringr)
 
 results_root_dir <- here("results")
-results_dir <- here("results", "biasadjust")
+results_dir <- file.path(results_root_dir, "biasadjust")
+
+ss_model_yr <- 2018
+ss_model_output_dir <- file.path(system.file(package = "pacifichakemse", mustWork = TRUE),
+                                 "extdata", paste0("SS3_", ss_model_yr))
+ss_model_data_csv_dir <- file.path(system.file(package = "pacifichakemse", mustWork = TRUE),
+                                   "extdata", "csv-data")
 
 fns <- c("MSErun_move_nofishing_nobiasadj",
          "MSErun_move_nofishing_biasadj",
@@ -14,23 +19,33 @@ plotnames <- c("0",
                "0.87",
                "0.5")
 
-run_mses(ss_extdata_dir = "SS32018",
-         nruns = 2,
-         simyears = 3,
+# A vector with one element for each scenario, which is additional proportion of the stock to be
+# moved from space 2 to space 1 (into Canada). If a single value instead of a vector, that value
+# will be used for all scenarios.
+movein_increases <- 0
+# A vector with one element for each scenario, which is additional proportion of the stock to be
+# moved from space 1 to space 2 (out of Canada). If a single value instead of a vector, that value
+# will be used for all scenarios.
+moveout_decreases <- 0
+
+# A vector with one element for each scenario, or a single value to use for all scenarios
+sel_changes <- 0
+
+b_futures <- c(0, 0.87, 0.5)
+
+run_mses(ss_model_output_dir = ss_model_output_dir,
+         ss_model_data_csv_dir = ss_model_data_csv_dir,
+         load_extra_mcmc = FALSE,
+         overwrite_ss_rds = FALSE,
+         n_runs = 1,
+         n_sim_yrs = 5,
+         b_futures = b_futures,
          fns = fns,
-         plotnames = plotnames,
-         om_params_seed = 12345,
-         multiple_season_data = list(c(nseason = 4,
-                                       nspace = 2,
-                                       bfuture = 0),
-                                     c(nseason = 4,
-                                       nspace = 2,
-                                       bfuture = 0.87),
-                                     c(nseason = 4,
-                                       nspace = 2,
-                                       bfuture = 0.5)),
+         plot_names = plotnames,
+         tacs = 0,
+         c_increases = movein_increases,
+         m_increases = moveout_decreases,
+         sel_changes = sel_changes,
          results_root_dir = results_root_dir,
          results_dir = results_dir,
-         nseason = 4,
-         nspace = 2,
-         bfuture = 0.5)
+         verbose = FALSE)
--- R/run_MSE_surveys.R
+++ R/run_MSE_surveys.R
@@ -19,6 +19,18 @@ plotnames <- c("Survey1",
                "Base scenario",
                "Survey3")
 
+# A vector with one element for each scenario, which is additional proportion of the stock to be
+# moved from space 2 to space 1 (into Canada). If a single value instead of a vector, that value
+# will be used for all scenarios.
+movein_increases <- 0
+# A vector with one element for each scenario, which is additional proportion of the stock to be
+# moved from space 1 to space 2 (out of Canada). If a single value instead of a vector, that value
+# will be used for all scenarios.
+moveout_decreases <- 0
+
+# A vector with one element for each scenario, or a single value to use for all scenarios
+sel_changes <- 0
+
 run_mses(ss_model_output_dir = ss_model_output_dir,
          ss_model_data_csv_dir = ss_model_data_csv_dir,
          load_extra_mcmc = FALSE,
