cdc66c7a422e85ba1dca6e141e17e2a86da07c9c
Chris Grandin <chris.grandin@gmail.com>
2020-11-20 02:35:19 -0800

    Add all attainment scenarios

---
+++ R/run_mse_attainment_3.R
@@ -0,0 +1,62 @@
+#library(pacifichakemse)
+load_all("../pacifichakemse")
+library(here)
+
+results_root_dir <- here("results")
+results_dir <- file.path(results_root_dir, "attainment")
+
+ss_model_yr <- 2018
+ss_model_output_dir <- file.path(system.file(package = "pacifichakemse", mustWork = TRUE),
+                                 "extdata", paste0("SS3_", ss_model_yr))
+ss_model_data_csv_dir <- file.path(system.file(package = "pacifichakemse", mustWork = TRUE),
+                                   "extdata", "csv-data")
+
+fns <- c("05_us_77_can_56",
+         "06_us_90_can_56")
+
+plotnames <- c("Recent (10-yr-avg) attainment",
+               "US high attainment (90%), 56% CAN")
+
+# List of vectors (of two) of the same length as the number of scenarios, one vector for each scenario.
+# For each vector of two e.g. c(a, b): a is the Canadian attainment proportion, b is the US attainment proportion
+attains <- list(c(0.56, 0.77),
+                c(0.56, 0.9))
+
+# List of vectors (of two) of the same length as the number of scenarios, one vector for each scenario.
+# For each vector of two e.g. c(a, b): the new catch in the OM is c_new * b + a
+# If instead of a 2-element vector, a single value is given, the expanded catch in the OM will be
+# c_new * 0.5 unless below catch_floor in which case it will be c_new = catch_floor.
+# To apply no TAC, use c(0, 1).
+# In any event, if the calculation is greater than c_new, c_new will be used instead
+tacs <- list(c(0, 1),
+             c(0, 1))
+
+# A vector with one element for each scenario, which is additional proportion of the stock to be
+# moved from space 2 to space 1 (into Canada). If a single value instead of a vector, that value
+# will be used for all scenarios.
+movein_increases <- 0
+# A vector with one element for each scenario, which is additional proportion of the stock to be
+# moved from space 1 to space 2 (out of Canada). If a single value instead of a vector, that value
+# will be used for all scenarios.
+moveout_decreases <- 0
+
+# A vector with one element for each scenario, or a single value to use for all scenarios
+sel_changes <- 0
+
+run_mses(ss_model_output_dir = ss_model_output_dir,
+         ss_model_data_csv_dir = ss_model_data_csv_dir,
+         load_extra_mcmc = FALSE,
+         overwrite_ss_rds = FALSE,
+         n_runs = 20,
+         n_sim_yrs = 30,
+         fns = fns,
+         plot_names = plotnames,
+         tacs = tacs,
+         attains = attains,
+         c_increases = movein_increases,
+         m_increases = moveout_decreases,
+         sel_changes = sel_changes,
+         results_root_dir = results_root_dir,
+         results_dir = results_dir,
+         catch_floor = 180000,
+         verbose = FALSE)
